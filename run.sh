168cc5e76cf510ba595e54d6bbaa0511cb02f8ff
alexp616 <alpan@ucsd.edu>
2024-07-27 15:46:26 -0700

    added script to facilitate running experiment

---
+++ run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+usage() {
+  echo "Usage: $0 -t 'time'"
+  exit 1
+}
+
+while getopts "t:" opt; do
+  case ${opt} in
+    t )
+      time_arg=$OPTARG
+      ;;
+    \? )
+      usage
+      ;;
+  esac
+done
+
+if [ -z "$time_arg" ]; then
+  usage
+fi
+
+julia --project -e 'using Pkg; Pkg.update()'
+julia --project -e "include(\"experiments/CalabiYau/RunCalabiYau.jl\"); run_experiment(time = $time_arg)" &
+julia --project -e "include(\"experiments/CalabiYau/RunCalabiYau.jl\"); run_experiment(time = $time_arg)" &
