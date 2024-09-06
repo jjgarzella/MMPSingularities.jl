b45693550a5b9383954c0e02f485a50ea98eac1c
alexp616 <101936321+alexp616@users.noreply.github.com>
2024-09-05 18:36:06 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,6 @@ usage() {
   exit 1
 }
 
-# Default number of processes if not specified
 num_processes=5
 
 while getopts "t:n:" opt; do
@@ -26,7 +25,6 @@ if [ -z "$time_arg" ]; then
   usage
 fi
 
-# Try to find the Julia executable and update the PATH if necessary
 julia_exec=$(which julia)
 if [ -z "$julia_exec" ]; then
   echo "Julia executable not found in PATH. Please ensure Julia is installed and available in your PATH."
@@ -35,14 +33,11 @@ fi
 
 echo "Using Julia at: $julia_exec"
 
-# Update Julia packages
 $julia_exec --project -e 'using Pkg; Pkg.update()'
 
-# Loop to run multiple processes
 for (( i=1; i<=num_processes; i++ ))
 do
   $julia_exec --project -e "include(\"experiments/CalabiYau/char7/RunCalabiYau.jl\"); run_experiment(time = $time_arg)" &
 done
 
-# Wait for all background processes to finish
 wait
