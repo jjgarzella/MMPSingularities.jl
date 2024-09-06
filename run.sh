af0fe9cfcb1229b198128ae2fe9c1397a63831b1
alexp616 <101936321+alexp616@users.noreply.github.com>
2024-09-05 18:35:04 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
 usage() {
-  echo "Usage: $0 -t 'time'"
+  echo "Usage: $0 -t 'time' -n 'num_processes'"
   exit 1
 }
 
-while getopts "t:" opt; do
+# Default number of processes if not specified
+num_processes=5
+
+while getopts "t:n:" opt; do
   case ${opt} in
     t )
       time_arg=$OPTARG
+      ;;
+    n )
+      num_processes=$OPTARG
       ;;
     \? )
       usage
@@ -20,9 +26,23 @@ if [ -z "$time_arg" ]; then
   usage
 fi
 
-julia --project -e 'using Pkg; Pkg.update()'
-julia --project -e "include(\"experiments/CalabiYau/char7/RunCalabiYau.jl\"); run_experiment(time = $time_arg)" &
-julia --project -e "include(\"experiments/CalabiYau/char7/RunCalabiYau.jl\"); run_experiment(time = $time_arg)" &
-julia --project -e "include(\"experiments/CalabiYau/char7/RunCalabiYau.jl\"); run_experiment(time = $time_arg)" &
-julia --project -e "include(\"experiments/CalabiYau/char7/RunCalabiYau.jl\"); run_experiment(time = $time_arg)" &
-julia --project -e "include(\"experiments/CalabiYau/char7/RunCalabiYau.jl\"); run_experiment(time = $time_arg)" &
+# Try to find the Julia executable and update the PATH if necessary
+julia_exec=$(which julia)
+if [ -z "$julia_exec" ]; then
+  echo "Julia executable not found in PATH. Please ensure Julia is installed and available in your PATH."
+  exit 1
+fi
+
+echo "Using Julia at: $julia_exec"
+
+# Update Julia packages
+$julia_exec --project -e 'using Pkg; Pkg.update()'
+
+# Loop to run multiple processes
+for (( i=1; i<=num_processes; i++ ))
+do
+  $julia_exec --project -e "include(\"experiments/CalabiYau/char7/RunCalabiYau.jl\"); run_experiment(time = $time_arg)" &
+done
+
+# Wait for all background processes to finish
+wait
